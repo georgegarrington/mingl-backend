35cc3f20e4aab53af18f06f2dc4676daff92f0bc
George Garrington <georgewgarrington@gmail.com>
2025-09-07 17:16:01 +0100

    POC of querying supabase

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -2,8 +2,11 @@ mod schema;
 
 use async_graphql::{EmptySubscription, http::GraphiQLSource};
 use async_graphql_poem::GraphQL;
-use poem::{IntoResponse, Route, Server, get, handler, listener::TcpListener, web::Html};
+use poem::{
+    EndpointExt, IntoResponse, Route, Server, get, handler, listener::TcpListener, web::Html,
+};
 pub use schema::{Mutation, Query};
+use sqlx::postgres::PgPoolOptions;
 
 #[handler]
 async fn graphiql() -> impl IntoResponse {
@@ -12,7 +15,29 @@ async fn graphiql() -> impl IntoResponse {
 
 #[tokio::main]
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let schema = async_graphql::Schema::build(Query, Mutation, EmptySubscription).finish();
+    // let conn = sqlx::Connection::connect("").await;
+
+    println!("CREATING POOL...");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://postgres:YJNhvX4pf9VjNu4f@db.zuejypwiozqqmmwvtsza.supabase.co:5432/postgres")
+        .await.unwrap();
+
+    println!("FETCHING...");
+
+    let (email, username): (String, String) =
+        sqlx::query_as("SELECT email, username FROM mingl_io.users")
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+
+    println!("EMAIL: {email}");
+    println!("UNAME: {username}");
+
+    let schema = async_graphql::Schema::build(Query, Mutation, EmptySubscription)
+        .data(pool)
+        .finish();
 
     let route = Route::new().at("/", get(graphiql).post(GraphQL::new(schema)));
 
